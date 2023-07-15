e7138aafea1274497fa377c3a3bdf786ae51a12e
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-15 14:42:32 -0700

    3-print_alphabets.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+/**
+* main - Goodness is the best
+* Return - please retun 0
+*/
+
+	int main(void)
+	{
+		char e;
+
+		char r;
+
+		e = 'a';
+
+		r = 'A';
+
+		while
+			(e <= 'z') { putchar(e);
+			e++;
+		}
+
+		while
+			(r <= 'Z') { putchar(r);
+			r++;
+		}
+
+		putchar('\n');
+
+		return (0);
+
+}
