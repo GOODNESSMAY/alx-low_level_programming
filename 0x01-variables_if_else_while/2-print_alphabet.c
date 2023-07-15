a38d2e8ac26798e8fd468da9a247812c4e29eaec
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-15 14:24:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+* main - I love Goodness
+* Return: why is that 0?
+*/
+
+int main(void)
+
+	{
+		char r;
+
+		r = 'a';
+
+		while
+			(r <= 'z') { putchar(r);
+			r++;
+}
+		putchar('\n');
+
+		return (0);
+
+}
