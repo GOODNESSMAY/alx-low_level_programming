7ef4b7adc47d720e0f764c0345c8780d07841c25
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-15 15:30:26 -0700

    print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+/**
+* main - Erick loves you Goodness!
+* Return: Why make his lie 0
+*/
+
+int main(void)
+
+{
+				char s;
+
+				s = 'a';
+
+				while
+					(s <= 'z') {
+					if ((s != 'q' && s != 'e') && s <= 'z')
+					putchar(s);
+					s++;
+}
+
+
+				putchar('\n');
+
+
+
+				return (0);
+
+}
