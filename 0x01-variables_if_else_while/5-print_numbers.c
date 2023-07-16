adaac6de8900b8d0b22a1b4759d88d38d064e5d1
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-16 08:31:49 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry coding is tough
+*Return: always 0
+*/
+
+int main(void)
+{
+
+int m;
+for (m = 0; m < 10; m++)
+
+printf("%d", m);
+putchar('\n');
+return (0);
+}
