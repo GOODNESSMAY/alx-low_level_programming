424b9e7bfa1f5d12017f67b44531847b601b7d40
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-16 09:19:51 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - starting point still coding on sunday
+* Return: 0 because it should be so
+*/
+int main(void)
+{
+char m;
+
+m = 'z';
+while
+
+(m >= 'a')
+{
+putchar(m);
+m--;
+}
+
+putchar('\n');
+return (0);
+}
