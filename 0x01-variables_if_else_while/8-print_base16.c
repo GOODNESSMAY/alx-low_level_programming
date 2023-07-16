7e4b5f2f7a3ed078479a0de5678b6c7c5916ec1c
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-16 09:42:09 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - starting point of another code
+* Return: 0 because i am saying so
+*/
+int main(void)
+{
+char n;
+int p;
+
+n = 'a';
+p = 0;
+while
+
+(p < 10)
+{
+putchar(p + '0');
+p++;
+}
+while
+(n <= 'f')
+{
+putchar(n);
+n++;
+}
+
+putchar('\n');
+return (0);
+}
