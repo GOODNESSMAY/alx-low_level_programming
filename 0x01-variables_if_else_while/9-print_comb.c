88c34fc5c76b4289fde9f437d11de54001710fe1
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-16 10:10:48 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - start point coding is fun now
+* Return: 0 always for the sake of erick
+*/
+
+int main(void)
+{
+int g;
+
+for (g = 0; g < 10; g++)
+
+{
+printf("%i", g);
+if (g < 9)
+printf(",");
+}
+
+printf("\n");
+
+return (0);
+}
