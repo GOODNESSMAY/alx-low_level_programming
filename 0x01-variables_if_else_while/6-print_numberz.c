b3b5e6c9315886ba82cdee131af68a3cc8b04c66
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-16 08:59:31 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - entry point coding on sunday is not cool
+* Return: 0 because i love zero tasks on sunday
+*/
+
+int main(void)
+{
+int e;
+for (e = 0; e < 10; e++)
+{
+putchar(e + '0');
+}
+putchar('\n');
+return (0);
+}
