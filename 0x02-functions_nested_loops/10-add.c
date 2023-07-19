22d8fe5b9fad0516578c6e5d860f9ddb98ab76e1
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-18 17:53:33 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+* add - adds two variables
+* @a1: the first number to be computed
+* @a2: the second number to be computed
+* Return: integer value
+*/
+int add(int a1, int a2)
+{
+	return (a1 + a2);
+}
