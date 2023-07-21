2a3741f021dac495d3b6a032f150414c2f7d5fd6
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-21 15:26:15 -0700

    3-print_num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - print the numbers 0 up to 9
+* return: the numbers from 0 to 9
+*/
+
+void print_numbers(void)
+
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+	{
+	_putchar(b + '0');
+	}
+	_putchar('\n')
+}
