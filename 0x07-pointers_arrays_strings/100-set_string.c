9ea2ac64fb9d970891f382c9c8386cbd74b01715
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-31 21:06:22 -0700

    ./100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+* set_string - write function of pointer char
+* @s: pointer
+* @to: points to array
+* Return: Always 0.
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
