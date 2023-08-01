1ccda89a8edbc0abb3f8023af8e7ee08ffb76615
GOODNESSMAY <goodnespeace651@gmail.com>
2023-07-31 18:03:04 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+* _memset - starting place
+* @s: points to a destination
+* @b: the constant value
+* @n: number of bytes to be changed
+* return: 0
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	unsigned int p = 0;
+
+	while (p < n)
+	p++;
+
+	s[p] = b;
+
+return (s);
+}
