6beacbc6d3045f84f2ef40239e1c63bddbf3d92d
GOODNESSMAY <goodnespeace651@gmail.com>
2023-08-03 01:58:58 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _puts_recursion - puts();
+* @s: input variables goody from char
+* Return: 0 (Success)
+*/
+
+void _puts_recursion(char *s)
+
+{
+
+if (*s)
+
+{
+
+_putchar(*s);
+
+_puts_recursion(s + 1);
+
+}
+
+else
+
+_putchar('\n');
+
+}
+
