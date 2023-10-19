05900bfc75a1b9308b0b60419aa173535f7546ac
GOODNESSMAY <goodnespeace651@gmail.com>
2023-10-19 14:29:06 -0700

    task 2 fixed

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,22 +1,61 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-    return (a + b);
+/**
+* add - plus two integers
+* @a: the first integer
+* @b: the second integer
+* Description: a code to call function with parameter a and b
+* Return: the sum of the two numbers
+*/
+int add(int a, int b)
+{
+	return (a + b);
 }
 
-int sub(int a, int b) {
-    return (a - b);
+/**
+ * sub - subtracts two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Description: a code to call function with parameter a and b
+ * Return: The difference of the two integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
 }
 
-int mul(int a, int b) {
-    return (a * b);
+/**
+ * mul - multiplies two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Description: a code to call function with parameter a and b
+ * Return: The product of the two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
 }
 
-int div(int a, int b) {
-    return (a / b);
+/**
+ * div - divides two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Description: a code to call function with parameter a and b
+ * Return: The quotient of the two integers
+ */
+int div(int a, int b)
+{
+	return (a / b);
 }
 
-int mod(int a, int b) {
-    return (a % b);
+/**
+ * mod - computes the remainder of two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Description: a code to call function with parameter a and b
+ * Return: The remainder of the two integers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
 }
-
